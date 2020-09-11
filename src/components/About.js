d079f96ad6da7e5f157c7bcffa328a8209c0914d
Luca Carrozzo <carrozzo.lc@gmail.com>
2020-09-11 15:39:44 +0200

    completed logic on about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+
+import VisibilitySensor from "react-visibility-sensor";
+
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const About = () => {
     const [readMore, setReadMore] = useState(false);
+    const [skillActive, setSkillActive] = useState(true);
+    const [skillsClasses, setSkillsClasses] = useState('skills');
+    const [skills, setSkills] = useState({
+        skill01: {
+            id: 10,
+            percentage: 0, 
+            skillTitle: 'Html5'
+        }, 
+        skill02: {
+            id: 11,
+            percentage: 0, 
+            skillTitle: 'Css3, Sass/Less'
+        }, 
+        skill03: {
+            id: 12,
+            percentage: 0,
+            skillTitle: 'Javascript'
+        }, 
+        skill04: {
+            id: 13,
+            percentage: 0,
+            skillTitle:'Responsive Design'
+        }, 
+        skill05: {
+            id: 14,
+            percentage: 0,  
+            skillTitle:'Web Design'
+        }
+    })
 
     const readMoreToggleHandler = () => {
         setReadMore(!readMore);
@@ -9,6 +43,61 @@ const About = () => {
 
     let attachedClasses = readMore ? ['about__rev-txt', 'open'] : ['about__rev-txt'];
 
+    const skillHandler = isVisible => {
+        if (isVisible) {
+            setSkillActive(false);
+            setSkillsClasses('skills show-el')
+            setSkills({
+                ...skills,
+                skill01: {
+                    ...skills.skill01,
+                    percentage: 99
+                },
+                skill02: {
+                    ...skills.skill02,
+                    percentage: 99
+                },
+                skill03: {
+                    ...skills.skill03,
+                    percentage: 90
+                },
+                skill04: {
+                    ...skills.skill04,
+                    percentage: 80
+                },
+                skill05: {
+                    ...skills.skill05,
+                    percentage: 90
+                }                 
+            });
+        }
+    }
+
+    let skillCircles = Object.keys(skills).map((item, index) => {
+        return (
+            <div className="skill" key={index}>
+                <div className="skill__box"> 
+                    <CircularProgressbar
+                        value={skills[item].percentage}
+                        text={`${skills[item].percentage}%`}
+                        strokeWidth={2}
+                        styles={buildStyles({
+                            strokeLinecap: 'butt',
+                            textSize: '24px',
+                            pathTransitionDuration: 0.9,
+                            pathColor: `rgba(25, 239, 204, ${skills[item].percentage / 100})`,
+                            textColor: skills[item].percentage === 0 ? '#ffffff' : 'rgb(85, 85, 85)',
+                            trailColor: '#ffffff'
+                        })}                                                        
+                    />              
+                </div>
+                <div className="skill__text">
+                    {skills[item].skillTitle}          
+                </div>
+            </div>
+        );
+    });    
+    
     return (
         <section className="about" id="about">
             <div className="container">
@@ -29,38 +118,13 @@ const About = () => {
                     <div className="btn-read" onClick={readMoreToggleHandler}>Read more</div>
                 </div>
 
-                <div className="skills">
-                    <div className="skill">
-                        <div className="skill__box skill01"></div>
-                        <div className="skill__text">
-                            Html5               
-                        </div>
+                <VisibilitySensor 
+                    onChange={skillHandler} 
+                    active={skillActive}>
+                    <div className={skillsClasses}>
+                        {skillCircles}
                     </div>
-                    <div className="skill">
-                        <div className="skill__box skill02"></div>
-                        <div className="skill__text">
-                            Css3, Sass/Less          
-                        </div>
-                    </div>
-                    <div className="skill">
-                        <div className="skill__box skill03"></div>
-                        <div className="skill__text">
-                            Javascript
-                        </div>
-                    </div>
-                    <div className="skill">
-                        <div className="skill__box skill04"></div>
-                        <div className="skill__text">
-                            Responsive Design
-                        </div>
-                    </div>
-                    <div className="skill">
-                        <div className="skill__box skill05"></div>
-                        <div className="skill__text">
-                            Web Design
-                        </div>
-                    </div>
-                </div>
+                </VisibilitySensor> 
 
             </div>
         </section>
