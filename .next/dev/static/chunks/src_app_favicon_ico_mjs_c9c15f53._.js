e340382ad1d10da0cebbbf2b78e843275e0df039
Luca Carrozzo <carrozzo.lc@gmail.com>
2026-01-18 11:38:08 +0100

    chore: remove .next from repository

--- .next/dev/static/chunks/src_app_favicon_ico_mjs_c9c15f53._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/7beb8_next_dist_4c1b44fb._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_layout_tsx_6f21f38e._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__0f0ba101._.css"
-],
-    source: "dynamic"
-});
